30aff888f6e82d1eb5882d67ce546886738954b5
annyziyun-cloud <annyziyun@gmail.com>
2026-09-23 07:12:59 +0800

    Add startGame function for loading game slot

--- start/start.js
+++ start/start.js
@@ -1,1 +1,8 @@
+function startGame(slotId) {
+  alert(`正在載入存檔 ${slotId}，冒險即將開始...`);
+  // 實作後續跳轉到實際遊戲場景 (例如: window.location.href = '../game/index.html')
+}
 
+function goBack() {
+  window.location.href = '../index.html';
+}
